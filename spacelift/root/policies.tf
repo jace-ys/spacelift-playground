3ddd063fe54bd495666f44e8ccfc1a0289a4a139
jace-ys <jace@duffel.com>
2024-04-09 16:56:15 +0100

    Fix policy

--- spacelift/root/policies.tf
+++ spacelift/root/policies.tf
@@ -1,6 +1,7 @@
 resource "spacelift_policy" "enforce-password-length" {
-  name   = "Enforce Password Length"
-  body   = file("${path.module}/policies/enforce-password-length.rego")
-  type   = "PLAN"
-  labels = ["autoattach:security"]
+  space_id = "root"
+  name     = "Enforce Password Length"
+  body     = file("${path.module}/policies/enforce-password-length.rego")
+  type     = "PLAN"
+  labels   = ["autoattach:security"]
 }
